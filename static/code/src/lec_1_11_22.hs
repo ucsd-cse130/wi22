2e4c74e95e5eaa27cd039ec0a86c86667fa3cc1a
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-01-13 10:59:45 -0800

    01-pdf

--- static/code/src/lec_1_11_22.hs
+++ static/code/src/lec_1_11_22.hs
@@ -1,2 +1,23 @@
 inc :: Int -> Int 
 inc x = x + 1
+
+
+
+{- 
+
+let SUM = \n -> ITE (ISZ n) ZERO (ADD n (SUM (DEC n)))
+
+
+let SUM = \n -> FST (n 
+                      (\p -> PAIR (ADD (FST p) (SND p)) (ADD (SND p) ONE))
+                      (PAIR ZERO ZERO)))
+
+def sum(n):
+
+    (res,i) = (0, 0)
+
+    repeat n times: 
+    
+    return res
+
+-}
