141359ae1460de21925591d76644327be54d77a9
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-03-01 09:27:39 -0800

    l7

---
+++ static/code/src/lec_3_1_22.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+
+module Lec_3_1_22 where
+
+import qualified Data.Map as M
+
+import Prelude hiding (lookup)
+
+inc :: Int -> Int
+inc x = x + 1
+
+
