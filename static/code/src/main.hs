54ed21de4933036c60dcbf74e71eba4bd17b61d7
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-03-10 10:53:02 -0800

    fix ieng6 bug

--- static/code/src/main.hs
+++ static/code/src/main.hs
@@ -1,3 +1,23 @@
+
+
+
 main :: IO ()
 main = putStrLn "Hello, 130!"
+IO
+-- type Recipe a = IO a
 
+-- main :: Recipe (
+
+main :: IO ()
+main = combine sayHello sayBye
+
+combine :: () -> IO () -> IO ()
+
+hellos = [sayHello, sayBye]
+hell2 = (sayHello, sayBye)
+
+sayHello :: IO ()
+sayHello = putStrLn "Hello hello, world!"
+
+sayBye :: IO ()
+sayBye   = putStrLn "Bye Bye, world!"
